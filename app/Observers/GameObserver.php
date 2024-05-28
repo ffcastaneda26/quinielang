00caeff5b1d805dd10c0330b95e67155b0506329
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-05-27 22:02:13 -0600

    Se  crea observador para modelo Game, se usará el método updated() para calificar pronósticos

--- app/Observers/GameObserver.php
+++ app/Observers/GameObserver.php
@@ -7,14 +7,6 @@ use App\Models\Game;
 class GameObserver
 {
     /**
-     * Handle the Game "created" event.
-     */
-    public function created(Game $game): void
-    {
-        //
-    }
-
-    /**
      * Handle the Game "updated" event.
      */
     public function updated(Game $game): void
@@ -28,27 +20,5 @@ class GameObserver
          */
     }
 
-    /**
-     * Handle the Game "deleted" event.
-     */
-    public function deleted(Game $game): void
-    {
-        //
-    }
 
-    /**
-     * Handle the Game "restored" event.
-     */
-    public function restored(Game $game): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Game "force deleted" event.
-     */
-    public function forceDeleted(Game $game): void
-    {
-        //
-    }
 }
