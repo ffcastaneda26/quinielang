814cc320833ab8c6395f61d0b0a5ca9e2e571805
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-06-12 14:26:10 -0600

    Seleccionar Jornada: Si es móvil pone un SELECT pero si llega a 640px cambia a botones

--- app/Livewire/SelectRound.php
+++ app/Livewire/SelectRound.php
@@ -10,11 +10,12 @@ class SelectRound extends Component
 {
 
     use FuncionesGenerales;
-
+    public $round_selected;
     public function mount(){
         $this->read_rounds();
         $round = new Round();
         $this->current_round = $round->read_current_round();
+       
     }
 
     public function render()
@@ -22,11 +23,19 @@ class SelectRound extends Component
 
         if(!$this->selected_round){
             $this->selected_round = $this->current_round;
+            $this->round_selected = $this->current_round->id;
         }
 
 
         $this->dispatch('read_round_games',$this->selected_round->id);
         return view('livewire.rounds.select-round');
 
+    }
+
+    public function round_select()
+    {
+        if($this->round_selected){
+            $this->selected_round = Round::findOrFail($this->round_selected);
+        }
     }
 }
--- resources/views/livewire/rounds/select-round.blade.php
+++ resources/views/livewire/rounds/select-round.blade.php
@@ -1,11 +1,20 @@
-<div class="flex justify-center mt-2">
+<div>
+    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="bg-slate-50">
             <div class="text-center uppercase">
-                <p>{{ __('Rounds') }}</p>
+                <p>{{ __('Round') }}</p>
             </div>
             <div class="d-flex justify-content-center text-center mt-2 mb-4">
+
+                <select class="w-full mt-3" 
+                        wire:model="round_selected"
+                        wire:change="round_select">
+                    <option value="">Seleccionar ronda</option>
                     @foreach ($rounds as $round)
-                        <button wire:click="select_round({{ $round->id }})"
+                        <option value="{{ $round->id }}">{{ $round->id }}</option>
+                    @endforeach
+                </select>
+                {{-- <button wire:click="select_round({{ $round->id }})"
                             class="cursor-pointer inline-flex items-center px-2 py-2 mx-2
                                     bg-gray-50   dark:bg-gray-200
                                     border border-black
@@ -17,9 +26,33 @@
                             {{ $selected_round->id == $round->id ? 'bg-green-400' : '' }}"
                             title="{{ $round->id }} {{ $current_round->id == $round->id ? __('Current Round')  : '' }}">
                             {{ $round->id }}
-                        </button>
-                    @endforeach
+                        </button> --}}
             </div>
         </div>
-</div>
+    </div>
 
+    <div class="flex justify-center mt-2 hidden sm:block">
+        <div class="bg-slate-50">
+            <div class="text-center uppercase">
+                <p>{{ __('Rounds') }}</p>
+            </div>
+            <div class="d-flex justify-content-center text-center mt-2 mb-4">
+                @foreach ($rounds as $round)
+                    <button wire:click="select_round({{ $round->id }})"
+                        class="cursor-pointer inline-flex items-center px-2 py-2 mx-2
+                                        bg-gray-50   dark:bg-gray-200
+                                        border border-black
+                                        rounded-full
+                                        font-bold text-xs text-black dark:text-gray-800
+                                        hover:bg-cyan-200 dark:hover:bg-white
+                                        focus:bg-green-200 dark:focus:bg-white
+                                {{ $current_round->id == $round->id ? 'bg-yellow-300' : '' }}
+                                {{ $selected_round->id == $round->id ? 'bg-green-400' : '' }}"
+                        title="{{ $round->id }} {{ $current_round->id == $round->id ? __('Current Round') : '' }}">
+                        {{ $round->id }}
+                    </button>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
