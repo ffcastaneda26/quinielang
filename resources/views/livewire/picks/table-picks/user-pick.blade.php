5aa8dbaa8550207d9cbd0c6ba9b5a1ed696c0ce5
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-06-12 17:42:26 -0600

    Tabla de Pronósticos: En encabezado se quita el círculo al perdedor y en detalle se pone en GRIS si se falló

--- resources/views/livewire/picks/table-picks/user-pick.blade.php
+++ resources/views/livewire/picks/table-picks/user-pick.blade.php
@@ -7,20 +7,23 @@
             @else
                 <div>
                     <img src="{{ Storage::url($pick->winner == 1 ? $game->local_team->logo : $game->visit_team->logo) }}"
-                        class="h-[30px] w-[30px] rounded-full mx-4">
+                        class="h-[30px] w-[30px] rounded-full mx-4  {{ $hit_game ? 'shadow-xl shadow-green-500' : 'grayscale' }}">
                     @if ($is_last_game)
                         {{ $visit_points . '-' . $local_points }}
                     @endif
                 </div>
 
             @endif
-
         </div>
 
-        @if ($has_result)
-            <img src="{{ $hit_game ? asset('images/afirmativo.png') : asset('images/negativo.png') }}"
-                class="h-[15px] w-[15px] rounded-full mx-4">
+        @if (env('PRINT_ICON_HIT_HAME_TABLE_PICKS', false))
+
+            @if ($has_result)
+                <img src="{{ $hit_game ? asset('images/afirmativo.png') : asset('images/negativo.png') }}"
+                    class="h-[15px] w-[15px] rounded-full mx-4">
+            @endif
         @endif
+
 
     </div>
 
--- resources/views/livewire/picks/tablepicks/table_picks_header.blade.php
+++ resources/views/livewire/picks/tablepicks/table_picks_header.blade.php
@@ -2,7 +2,7 @@
 <div class="w-full flex items-center mt-2">
     <div class="w-full grid grid-cols-12 border">
         {{-- <div class="w-auto col-span-2 border border-blue-900">NOMBRE</div> --}}
-        <div class="w-auto col-span-2 border border-blue-900 flex items-center justify-center">NOMBRE</div>
+        <div class="w-auto col-span-2 border border-blue-900 flex items-center justify-center uppercase">{{ __('Name') }}</div>
         <div class="w-auto col-span-7 border border-blue-900 text-center">
             <div class="flex flex-row">
                 @foreach ($round_games as $game)
@@ -10,12 +10,12 @@
                         @if($game->local_points || $game->visit_points)
                             <img src="{{ Storage::url($game->visit_team->logo) }}"
                                 class="h-[25px] w-[25px] rounded-full mx-4 border-solid  shadow-xl
-                                    {{  $game->visit_points > $game->local_points ? 'shadow-green-500 h-[35px] w-[35px] border-4 border-green-500'
-                                                                                  : 'shadow-red-500 border-4 border-red-500'}} " >
+                                    {{  $game->visit_points > $game->local_points ? 'shadow-green-500 h-[35px] w-[35px]'
+                                                                                  : ''}} " >
                             <img src="{{ Storage::url($game->local_team->logo) }}"
                                 class="h-[25px] w-[25px] rounded-full mx-4 shadow-xl
-                                    {{  $game->local_points > $game->visit_points ? 'shadow-green-500 h-[35px] w-[35px] border-4 border-green-500'
-                                                                                  : 'shadow-red-500 border-4 border-red-500'}}">
+                                    {{  $game->local_points > $game->visit_points ? 'shadow-green-500 h-[35px] w-[35px]'
+                                                                                  : ''}}">
                             <span class="text-center">
                                 <label class="rounded-full">
                                         {{ $game->visit_points }}
@@ -37,7 +37,7 @@
             </div>
         </div>
 
-        <div class="w-auto col-span-1 border border-blue-900 flex items-center justify-center">ACIERTOS</div>
+        <div class="w-auto col-span-1 border border-blue-900 flex items-center justify-center uppercase">{{ __('Hits') }}</div>
         @if(env('PRINT_ACUMULATED_BY_ROUND',false))
             <div class="w-auto col-span-1 border border-blue-900 flex items-center justify-center">ACUMULADO</div>
         @endif
