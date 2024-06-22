48eaf14d283122630644d23cd3144b36ef6f6406
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-06-21 22:53:42 -0600

    Renglón de marcador del último partido con clase especial a 0.65 rem

--- resources/views/livewire/picksgames/index.blade.php
+++ resources/views/livewire/picksgames/index.blade.php
@@ -1,4 +1,9 @@
 <div wire:poll>
+    <style>
+        .text-xxs {
+            font-size: 0.65rem;
+        }
+    </style>
     <div class="container mx-auto  bg-gray-100 rounded-md px-4 py-2 my-2">
         <div class="grid grid-cols-9">
             <div class="col-span-2 text-xs">
@@ -6,27 +11,51 @@
                 {{ $game->game_date->format('h:i A') }}
             </div>
 
-            <div class="col-span-2 flex flex-row">
-                <img src="{{ Storage::url($game->visit_team->logo) }}" class="h-[30px] w-[30px] rounded-full mr-2">
-                <span>{{ $game->visit_team->short }}</span>
-            </div>
-            {{-- @include('livewire.picksgames.pick_visit') --}}
+            @if ($is_game_tie_breaker)
+                <div class="flex flex-col w-1/6">
+                    <img src="{{ Storage::url($game->visit_team->logo) }}" class="h-[30px] w-[30px] rounded-full mr-2">
+                </div>
 
-            {{-- Pronóstico Visita --}}
+                <div class="flex flex-col w-1/6">
+                    <input type='number' wire:model="visit_points" wire:change="update_points" wire:blur="update_points"
+                        min=0 max=99
+                        class="text-xxs {{ $errors->has('visit_points') ? 'border border-red-500 border-double' : '' }}
+                        {{ $allow_pick ? '' : 'bg-slate-200' }}"
+                        {{ $allow_pick ? '' : 'disabled' }} />
+                </div>
 
-            @include('livewire.picksgames.visit_pick')
+                <div class="flex flex-col w-1/6">
+                    @if ($game_has_result)
+                        <img src="{{ $hit_game ? asset('images/afirmativo.png') : asset('images/negativo.png') }}"
+                            width="25" height="25">
+                    @else
+                        <img src="{{ asset('images/' . env('IMG_TO_YET_PICK_GAME', 'reloj.jpg')) }}" width="25"
+                            height="25">
+                    @endif
+                </div>
 
-            @include('livewire.picksgames.pick_icono_acerto')
+                <div class="flex flex-col w-1/6">
+                    <input type='number' wire:model="local_points" wire:change="update_points"
+                        wire:blur="update_points" min=0 max=99
+                        class="text-xxs {{ $errors->has('local_points') ? 'border border-red-500 border-double' : '' }}
+                        {{ $allow_pick ? '' : 'bg-slate-200' }}"
+                        {{ $allow_pick ? '' : 'disabled' }} />
+                </div>
+                <div class="flex flex-col w-1/6">
+                    <img src="{{ Storage::url($game->local_team->logo) }}" class="h-[30px] w-[30px] rounded-full mr-2">
 
-            {{-- Pronóstico Loca --}}
-            @include('livewire.picksgames.local_pick')
+                </div>
+            @else
+                @include('livewire.picksgames.pick_visit')
+                @include('livewire.picksgames.visit_pick')
 
-            <div class="col-span-2">
-                <img src="{{ Storage::url($game->local_team->logo) }}" class="h-[30px] w-[30px] rounded-full mr-2">
-                <span class="{{ $is_game_tie_breaker ? 'hidden' : '' }}">{{ $game->local_team->short }}</span>
-            </div>
+                @include('livewire.picksgames.pick_icono_acerto')
 
-            {{-- @include('livewire.picksgames.pick_local') --}}
+                @include('livewire.picksgames.local_pick')
+
+                @include('livewire.picksgames.pick_local')
+            @endif
+
         </div>
     </div>
 </div>
--- resources/views/livewire/picksgames/pick_visit.blade.php
+++ resources/views/livewire/picksgames/pick_visit.blade.php
@@ -1,6 +1,6 @@
     {{-- Datos de la visita --}}
-    <div class="col-span-2 flex flex-row items-center gap-10">
+    <div class="col-span-2 flex flex-col sm:flex-row items-center gap-4 text-xs">
         <img src="{{ Storage::url($game->visit_team->logo) }}" class="h-[30px] w-[30px] rounded-full mr-2">
-        <div class="{{ $is_game_tie_breaker ? 'hidden': '' }}">{{ $game->visit_team->short }}</div>
+        <div class="{{ $is_game_tie_breaker ? 'hidden' : '' }}">{{ $game->visit_team->short }}</div>
     </div>
 
