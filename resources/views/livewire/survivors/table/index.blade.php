8da3e7843616527351030ead18a14c7ae7747b1e
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-07-28 15:54:28 -0600

    Tabla de survivors sin llamar a movil o no movil

--- resources/views/livewire/survivors/table/index.blade.php
+++ resources/views/livewire/survivors/table/index.blade.php
@@ -1,37 +1,45 @@
 <div class="container">
-    <div class="mx-auto">
+    <div class="mx-auto mt-5">
         @foreach ($users as $user)
-            {{-- <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden"> --}}
-                <table class="w-full border">
-                        @if($loop->index == 0)
-                            <thead>
-                                <tr>
-                                    <th class="text-center font-bold">{{ __('Name') }}</th>
-                                    @foreach ($rounds as $round)
-                                        <th class="text-center text-sm">{{ $round->id }}</th>
-                                    @endforeach
-                                </tr>
-                            </thead>
-                        @endif
-                    <tbody>
-                        <tr>
-                            <td class="text-center font-bold">{{ $user->alias }}</td>
+            <table class="w-full border">
+                @if ($loop->index == 0)
+                    <thead>
+                        <tr class="text-center font-bold">
+                            <td colspan="{{ $rounds->count() + 1 }}" align="center">
+                                <img src="{{ asset('images/survivor_no_seleccionado.png') }}" class="w-4 rounded-full grayscale">
+                                <span class="text-xxs"> Indica que no se seleccionó equipo</span>
+                            </td>
+                        </tr>
+                        <tr class="text-center font-bold bg-black text-white">
+                            <th style="width: 5%;">{{ __('Name') }}</th>
                             @foreach ($rounds as $round)
-                                <td class="text-center">
-                                    @php
-                                        $user_survivor = $user->survivors->firstWhere('round_id', $round->id);
-                                    @endphp
-                                    @if ($user_survivor)
-                                        <img src="{{ Storage::url($user_survivor->team->logo) }}" class="w-8">
-                                    @else
-                                        <img src="{{ asset('images/survivor_no_seleccionado.png') }}" class="w-8">
-                                    @endif
-                                </td>
+                                <th class="text-center text-sm">{{ $round->id }}</th>
                             @endforeach
                         </tr>
-                    </tbody>
-                </table>
-            {{-- </div> --}}
+                    </thead>
+                @endif
+                <tbody>
+                    <tr class="border-black">
+                        <td style="width: 5%;" class="text-start truncate">{{ $user->alias }}</td>
+                        @foreach ($rounds as $round)
+                            <td class="text-center border border-black equal-width" align="center">
+                                @php
+                                    $user_survivor = $user->survivors->firstWhere('round_id', $round->id);
+                                @endphp
+                                @if ($user_survivor)
+                                    <img src="{{ Storage::url($user_survivor->team->logo) }}"
+                                        class="w-6 rounded-full {{ $user_survivor->survive ? 'shadow-xl shadow-green-500' : 'grayscale' }}"
+                                        style="margin: 0 auto;">
+                                @else
+                                    <img src="{{ asset('images/survivor_no_seleccionado.png') }}"
+                                        class="w-6 rounded-full grayscale"
+                                        style="margin: 0 auto;">
+                                @endif
+                            </td>
+                        @endforeach
+                    </tr>
+                </tbody>
+            </table>
         @endforeach
     </div>
 </div>
