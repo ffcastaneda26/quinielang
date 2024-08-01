fbae927721b1330198f7ad9b2206c6864446b5b1
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-08-01 10:29:08 -0600

    Se quita menú de navegación superior  se deja en el cuerpo de la pantalla

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 <div class="flex justify-center items-center w-full flex-1">
-    <img src="{{ asset('images/jugador.png') }}" class="rounded-full w-72">
+    <img src="{{ asset('images/jugador.png') }}" class="rounded-md w-36">
 </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,19 +1,11 @@
 <x-app-layout>
-    <x-slot name="header">
-
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            <div class="flex">
-                {{ env('APP_NAME','QUINIELA 2024') }}
-            </div>
-
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+    <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+           <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 <x-welcome />
             </div>
+
+
         </div>
     </div>
 </x-app-layout>
