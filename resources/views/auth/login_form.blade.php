4faec7dd89fcd0b6d02b5bb42da136f228ee647b
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-08-01 09:33:24 -0600

    Se cambian imagenes  y en página principal se agrega formulario par aingresar

---
+++ resources/views/auth/login_form.blade.php
@@ -0,0 +1,32 @@
+<form method="POST" action="{{ route('login') }}">
+    @csrf
+
+    <div>
+        <x-label for="login" value="{{ __('Email/Username') }}" />
+        <x-input id="login" class="block mt-1 w-full" type="text" name="login" :value="old('login')" required autofocus autocomplete="username" />
+    </div>
+
+    <div class="mt-4">
+        <x-label for="password" value="{{ __('Password') }}" />
+        <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+    </div>
+
+    <div class="block mt-4 hidden">
+        <label for="remember_me" class="flex items-center">
+            <x-checkbox id="remember_me" name="remember" checked/>
+            <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+        </label>
+    </div>
+
+    <div class="flex items-center justify-end mt-4">
+        @if (Route::has('password.request'))
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                {{ __('Forgot your password?') }}
+            </a>
+        @endif
+
+        <x-button class="ms-4">
+            {{ __('Log in') }}
+        </x-button>
+    </div>
+</form>
