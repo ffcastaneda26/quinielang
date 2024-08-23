f52738b5da6781cd19473e5074a1a19cd381d992
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-08-23 10:09:27 -0600

    Tabla de pronósticos: Se pasa a archivos separados el encabezado de partidos y el detalle de pronósticos del usuario

--- resources/views/livewire/picks/tablepicks/table_picks_detail.blade.php
+++ resources/views/livewire/picks/tablepicks/table_picks_detail.blade.php
@@ -1,3 +1,42 @@
-@include('livewire.picks.tablepicks.table_picks_detail_movil')
+{{-- Pronósticos por Usuario --}}
+@foreach ($users as $user )
+    <div class="w-full grid grid-cols-12 border bg-white">
+        <div class="col-span-2 flex items-center text-center border font-bold text-xxs bg-white text-black sm:text-sm md:text-lg lg:text-2xl">{{ $user->alias }}</div>
+        <div class="col-span-9">
+            <div class="flex flex-row gap-1 justify-between items-center border bg-white text-black">
+                @foreach ($user->picks->sortBy('game.game_date') as $pick)
+                    {{-- <div class="col-span-1 gap-2 justify-center items-center"> --}}
+                    <div class="col-span-1 flex items-center border justify-center font-bold ">
+                        @if ($pick->game->allow_pick())
+                            <img src="{{ asset('images/' . env('IMG_TO_YET_PICK_GAME', 'reloj.png')) }}"
+                                class="w-4 h-4 rounded-full">
+                        @else
+                            <div class="flex flex-col justify-center items-center border text-xxs sm:text-sm md:text-lg lg:text-2xl">
+                                <div class="flex justify-center items-center">
+                                        <img src="{{ Storage::url($pick->winner == 1 ? $pick->game->local_team->logo : $pick->game->visit_team->logo) }}"
+                                            class="w-4 h-4 rounded-full shadow-md  {{ $pick->winner == $pick->game->winner ? 'shadow-green-500' : 'grayscale' }}"
+                                        >
+                                </div>
 
-@include('livewire.picks.tablepicks.table_picks_detail_no_movil')
+                                @if($pick->winner == $pick->game->winner)
+                                    <div class="flex flex-col justify-center items-centertext-center">
+                                        <img src="{{ asset('images/afirmativo.png') }}"
+                                        class="w-1">
+                                    </div>
+                                @endif
+                            </div>
+                        @endif
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
+        <div class="col-span-1 flex items-center border justify-center font-bold text-xxs bg-white text-black sm:text-sm md:text-lg lg:text-2xl">
+            {{ $user->positions->first() ? $user->positions->first()->hits : ''}}
+        </div>
+
+    </div>
+@endforeach
+<div class="flex justify-end">
+    {{ $users->links() }}
+</div>
--- resources/views/livewire/picks/tablepicks/table_picks_header.blade.php
+++ resources/views/livewire/picks/tablepicks/table_picks_header.blade.php
@@ -1,4 +1,36 @@
-{{-- Móvil  --}}
-@include('livewire.picks.tablepicks.table_picks_header_movil')
-{{-- Ancho mayor a 640px --}}
-@include('livewire.picks.tablepicks.table_picks_header_no_movil')
+<div class="w-full grid grid-cols-12 border bg-white text-black">
+    <div class="col-span-2 flex items-center text-center text-xxs font-bold bg-white text-black">{{ __('Name') }}</div>
+    <div class="col-span-9 bg-white text-black">
+        <div class="flex flex-row gap-1 justify-between items-center border bg-white text-black">
+            @foreach ($round_games as $game)
+                <div class="col-span-1 gap-1">
+                        @if ($game->local_points || $game->visit_points)
+                            <img src="{{ Storage::url($game->visit_team->logo) }}"
+                                class="w-4 h-4 rounded-full border-solid  shadow-xl
+                                {{ $game->visit_points > $game->local_points ? 'shadow-green-500 w-35 h-35' : '' }} ">
+                            <div class="text-xxs {{ $game->winner == 2 ? 'text-green-500 font-bold' : 'text-red-500'  }}">
+                                {{ $game->visit_points }}
+                            </div>
+                            <img src="{{ Storage::url($game->local_team->logo) }}"
+                                class="w-4 h-4 rounded-full border-solid  shadow-xl
+                                {{ $game->local_points > $game->visit_points ? 'shadow-green-500 w-35 h-35' : '' }} ">
+
+                            <div class="text-xxs {{ $game->winner == 1 ? 'text-green-500 font-bold' : 'text-red-500'  }}">
+                                {{ $game->local_points }}
+                            </div>
+                        @else
+                            <img src="{{ Storage::url($game->visit_team->logo) }}" class="w-4 h-4 rounded-full">
+                            @if($round_has_games_played)
+                                <br>
+                            @else
+                                <hr>
+                            @endif
+                            <img src="{{ Storage::url($game->local_team->logo) }}" class="w-4 h-4 rounded-full">
+                        @endif
+                </div>
+            @endforeach
+
+        </div>
+    </div>
+    <div class="col-span-1 flex items-center text-center font-bold justify-center ml-2 text-xxs bg-white text-black sm:text-sm md:text-lg lg:text-2xl">AC</div>
+</div>
