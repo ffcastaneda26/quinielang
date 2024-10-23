89008b223b25597d6dc7bc9c88ede48db9de94df
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-10-23 15:37:46 -0600

    En pronósticos se agrega el resultado del partido si es que ya tiene marcador

--- resources/views/livewire/picksgames/pick_local.blade.php
+++ resources/views/livewire/picksgames/pick_local.blade.php
@@ -1,4 +1,5 @@
    {{-- Datos del local --}}
-   <div class="col-span-2 flex flex-col sm:flex-row items-center gap-10 text-xs">
+   <div class="col-span-2 flex  sm:flex-row items-center gap-10 text-xs">
+        {{  $game_has_result ? $game->local_points : '' }}
         <img src="{{ Storage::url($local_logo) }}" class="h-[30px] w-[30px] rounded-full mr-2">
    </div>
