a91bd06dc366d5348d7087028328a0b2c28d11ce
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2025-09-04 09:29:14 -0600

    Cambios en las reglas de la quiniela en página de bienvenida una vez conectado

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,6 +1,5 @@
 <link rel="stylesheet" href="{{ asset('css/marquee.css') }}">
-<div
-    class="p-6 lg:p-8 bg-white text-black">
+<div class="p-6 lg:p-8 bg-white text-black">
 
     {{-- <div class="position-relative marquee-container d-none d-sm-block">
         <div class="marquee d-flex justify-content-around">
@@ -21,12 +20,16 @@
             <p class="mt-4 text-black text-xs leading-relaxed">
             <div>
                 <ul class="list-disc">
-                    <li>En la sección de "Pronósticos" Cada Participante selecciona entre Local y Visita pronosticando al Ganador.</li>
-                    <li>En el último partido de cada Jornada se debe pronosticar Puntos Local y Puntos Visita, este es el Partido Desempate y los criterios se listan en la sección de desempate.</li>
-                    <li>La Selección de Local/Visita del Partido Desempate cambia automáticamente después de guardar el
-                        pronóstico de la jornada.</li>
-                    <li>Cada pronóstico puede ser modificado hasta 5 minutos antes de la hora de inicio del partido.</li>
-                    <li>Los pronósticos que han sido bloqueados para modificación simultáneamente se hacen visibles para el resto de los participantes en "Tabla Pronósticos".</li>
+                    <li>En la sección de "Pronósticos" Cada Participante selecciona entre Local y Visita pronosticando
+                        al Ganador.</li>
+                    <li>En el último partido de cada Jornada se debe pronosticar Puntos Local y Puntos Visita, este es
+                        el Partido Desempate y los criterios se listan en la sección de desempate.</li>
+                    <li>La Selección de Local/Visita del Partido Desempate cambia automáticamente después de modificar
+                        los puntos local o visita.</li>
+                    <li>Cada pronóstico puede ser modificado hasta 5 minutos antes de la hora de inicio del partido.
+                    </li>
+                    <li>Los pronósticos que han sido bloqueados para modificación simultáneamente se hacen visibles para
+                        el resto de los participantes en "Tabla Pronósticos".</li>
                     <li>Gana quien más puntos obtengan al final de cada Jornada y/o toda la temporada (Acumulado).</li>
                 </ul>
             </div>
@@ -56,11 +59,17 @@
                 <li>1 Punto. Partidos Playoff Ronda Divisional (4 Partidos).</li>
                 <li>2 Puntos. Partidos Final de Conferencia NFC y AFC (2 Partidos).</li>
                 <li>3 Puntos. Super Bowl (1 Partido)</li>
+
             </ul>
             <p class="ms-3 mt-2 text-md font-semibold text-black">
                 Las puntuaciones aplican tanto en Jornada como Acumulado.
             </p>
 
+            <p class="ms-3 mt-2 text-md  text-black">
+                <strong>Nota:</strong> En las Finales de Conferencia y el Super Bowl, que valen 2 y 3 puntos respectivamente, se pueden elegir
+                equipos distintos. Según las selecciones y el resultado, un partido puede dar entre 0 y 3 puntos. El
+                último partido del Super Bowl será el criterio de desempate en la Jornada 19 y, como los MNF, otorgará
+                solo 1 punto.
             </p>
         </div>
 
@@ -73,11 +82,11 @@
                 </h2>
             </div>
             <p class="mt-4 text-black text-sm leading-relaxed">
-                <ul class="list-disc">
-                    <li>Un Ganador por cada Jornada. (18 Temporada Regular + 1 de Playoffs J19).</li>
-                    <li>1°, 2°, 3°, 4° y 5° Lugar en el Acumulado de Toda la Temporada incluyendo Super Bowl.</li>
-                    <li>2 Ganadores de Survivor</li>
-                </ul>
+            <ul class="list-disc">
+                <li>Un Ganador por cada Jornada. (18 Temporada Regular + 1 de Playoffs J19).</li>
+                <li>1°, 2°, 3°, 4° y 5° Lugar en el Acumulado de Toda la Temporada incluyendo Super Bowl.</li>
+                <li>2 Ganadores de Survivor</li>
+            </ul>
             </p>
         </div>
 
@@ -116,7 +125,7 @@
             <p class="mt-4 ml-5 text-black text-sm leading-relaxed">
             <ul>
                 <li>1. Más Puntos en toda la temporada.</li>
-                <li>2. Más Puntos en los partidos de desempate (El SB Vale triple).</li>
+                <li>2. Más Puntos en los partidos de desempate (El SB Vale sencillo).</li>
                 <li>3.Menor Error Total acumulado durante los 19 juegos de desempate. (Error Total Jornada 1 +
                     J2+J3...J19)
                     <br>
@@ -141,12 +150,14 @@
             </div>
 
             <p class="mt-4 ml-5 text-black text-sm leading-relaxed">
-                <ul class="list-disc">
-                    <li>Todos los Cálculos de Error en puntos son valores Absolutos.</li>
-                    <li>En caso de seguir empates tras aplicar los criterios, las bolsas de las posiciones empatadas se repartirán entre el número de participantes empatados.</li>
-                    <li>Si un partido termina empatado no genera puntos en la semana o acumulado, si un partido de desempate termina en empate la regla 5 de Jornadas aplica a los puntos de local.</li>
-                    <li>Lo no contemplados dentro de los criterios quedarán sujeto a consideración del organizador.</li>
-                </ul>
+            <ul class="list-disc">
+                <li>Todos los Cálculos de Error en puntos son valores Absolutos.</li>
+                <li>En caso de seguir empates tras aplicar los criterios, las bolsas de las posiciones empatadas se
+                    repartirán entre el número de participantes empatados.</li>
+                <li>Si un partido termina empatado no genera puntos en la semana o acumulado, si un partido de desempate
+                    termina en empate la regla 5 de Jornadas aplica a los puntos de local.</li>
+                <li>Lo no contemplados dentro de los criterios quedarán sujeto a consideración del organizador.</li>
+            </ul>
             </p>
 
             <div class="flex items-center">
@@ -169,29 +180,36 @@
             </div>
 
             <p class="mt-4 ml-5 text-black text-sm leading-relaxed">
-                <ul class="list-disc">
-                    <li>
-                        En la sección de <strong>"Survivor"</strong> cada Partitipante seleccionará un equipo por Jornada (No se puede repetir equipo en toda la Temporada Regular).
-                    </li>
-                    <li>Si tu equipo seleccionado gana, "sobrevives" la Jornada y continuas con vida para la siguiente jornada.</li>
-                    <li>Si tu equipo seleccionado empata o pierde quedas eliminado del premio Survivor y continuas participando como Zombie.</li>
-                    <li>Si no hiciste selección en la Jornada o duplicaste un equipo entonces quedas eliminado del premio Survivor y continuas participando como Zombie.</li>
-                    <li>Cada Jornada los equipos estarán disponibles para selección cuando se cumplan los siguientes requisitos:</li>
-                        <div class="flex justify-end">
-                            <ul style="list-style-type:decimal" class="mt-2">
-                                <li>El equipo tenga partido en la Jornada (No esté de descanso)</li>
-                                <li>El Equipo No esté seleccionado en otra Jornada (anterior o futura)</li>
-                                <li>El Partido no haya iniciado o esté a 5 minutos de iniciar.</li>
-                                <li>La Jornada siga siendo editable.</li>
-                            </ul>
-                        </div>
+            <ul class="list-disc">
+                <li>
+                    En la sección de <strong>"Survivor"</strong> cada Partitipante seleccionará un equipo por Jornada
+                    (No se puede repetir equipo en toda la Temporada Regular).
+                </li>
+                <li>Si tu equipo seleccionado gana, "sobrevives" la Jornada y continuas con vida para la siguiente
+                    jornada.</li>
+                <li>Si tu equipo seleccionado empata o pierde quedas eliminado del premio Survivor y continuas
+                    participando como Zombie.</li>
+                <li>Si no hiciste selección en la Jornada o duplicaste un equipo entonces quedas eliminado del premio
+                    Survivor y continuas participando como Zombie.</li>
+                <li>Cada Jornada los equipos estarán disponibles para selección cuando se cumplan los siguientes
+                    requisitos:</li>
+                <div class="flex justify-end">
+                    <ul style="list-style-type:decimal" class="mt-2">
+                        <li>El equipo tenga partido en la Jornada (No esté de descanso)</li>
+                        <li>El Equipo No esté seleccionado en otra Jornada (anterior o futura)</li>
+                        <li>El Partido no haya iniciado o esté a 5 minutos de iniciar.</li>
+                        <li>La Jornada siga siendo editable.</li>
+                    </ul>
+                </div>
 
-                </ul>
+            </ul>
             </p>
 
             <div class="flex items-center">
                 <p class="mt-4 ml-50 font-bold italic">
-                    *Los pronósticos survivor de cada Jornada se hacen simultaneamente visibles y no editables para todos los participantes 5 minutos ante de iniciar el primer partido de la Jornada en donde algún participante tenga equipo seleccionado.
+                    *Los pronósticos survivor de cada Jornada se hacen simultaneamente visibles y no editables para
+                    todos los participantes 5 minutos ante de iniciar el primer partido de la Jornada en donde algún
+                    participante tenga equipo seleccionado.
                 </p>
             </div>
 
@@ -207,13 +225,18 @@
             </div>
 
             <p class="mt-4 ml-5 text-black text-sm leading-relaxed">
-                <p>La competencia contempla que habrá al menos 2 premios y existen diferentes escenarios:</p>
-                <ul class="list-disc">
-                    <li>a) Ganador Survivor Único, si al término de una Jornada existe un sólo sobreviviente, éste declara ganador Survivor y el resto participan por el premio de Zombies (2do Lugar).</li>
-                    <li>b) Sin Ganador Survivor, si al término de una Jornada no hay sobrevivente todos los participantes compiten como Zombies. (Esto sucede cuando la jornada inicia con 2+ Sobrevivientes y termina sin sobrevivientes)</li>
-                    <li>c) Ganadores Multi Survivor, si al término de la Temporada Regular hay 2+ Sobrevivientes estos serán ganadores, el desempate se juega en playoffs, de suceder los 2 premios serían a Surviors y no hay premiación de Zombies.</li>
+            <p>La competencia contempla que habrá al menos 2 premios y existen diferentes escenarios:</p>
+            <ul class="list-disc">
+                <li>a) Ganador Survivor Único, si al término de una Jornada existe un sólo sobreviviente, éste declara
+                    ganador Survivor y el resto participan por el premio de Zombies (2do Lugar).</li>
+                <li>b) Sin Ganador Survivor, si al término de una Jornada no hay sobrevivente todos los participantes
+                    compiten como Zombies. (Esto sucede cuando la jornada inicia con 2+ Sobrevivientes y termina sin
+                    sobrevivientes)</li>
+                <li>c) Ganadores Multi Survivor, si al término de la Temporada Regular hay 2+ Sobrevivientes estos serán
+                    ganadores, el desempate se juega en playoffs, de suceder los 2 premios serían a Surviors y no hay
+                    premiación de Zombies.</li>
 
-                </ul>
+            </ul>
             </p>
 
             <div class="flex items-center mt-4">
@@ -223,21 +246,24 @@
                 </h2>
             </div>
             <p class="flex items-center  mt-4 ml-5 text-black text-sm">
-                <ul class="list-disc">
-                    <li>Zombies son los participantes eliminados del Survivor, estos seguirán participando por el resto de la temporada a la espera de que no haya Survivors Múltiples para obtener premio.</li>
-                    <li>
-                        Cada Jornada donde se acierta Ganador es un punto, el Zombie con más puntos en toda la temporada Regular será el ganador, en caso de empate se jugará un torneo Survivor en Playoffs y de seguir empatados aplican los siguientes criterios:
-                    </li>
-                    <div class="flex justify-end">
-                        <ul style="list-style-type:decimal" class="mt-2">
-                            <li>Más Jornadas como sobreviviente.</li>
-                            <li>Racha de victoria más larga después de hacerse zombie. (2da derrota)</li>
-                            <li>Más equipos seleccionados Visitantes</li>
-                        </ul>
+            <ul class="list-disc">
+                <li>Zombies son los participantes eliminados del Survivor, estos seguirán participando por el resto de
+                    la temporada a la espera de que no haya Survivors Múltiples para obtener premio.</li>
+                <li>
+                    Cada Jornada donde se acierta Ganador es un punto, el Zombie con más puntos en toda la temporada
+                    Regular será el ganador, en caso de empate se jugará un torneo Survivor en Playoffs y de seguir
+                    empatados aplican los siguientes criterios:
+                </li>
+                <div class="flex justify-end">
+                    <ul style="list-style-type:decimal" class="mt-2">
+                        <li>Más Jornadas como sobreviviente.</li>
+                        <li>Racha de victoria más larga después de hacerse zombie. (2da derrota)</li>
+                        <li>Más equipos seleccionados Visitantes</li>
+                    </ul>
 
-                    </div>
+                </div>
 
-                </ul>
+            </ul>
             </p>
         </div>
     </div>
